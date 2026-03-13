39b54740e71d3ebb733adef1fcc01b71d69fef38
arjun121094 <arjun121094@gmail.com>
2026-03-13 10:59:35 +0530

    Refactor movies model: update SQL configuration and add YAML schema with unit tests

--- models/staging/movies.sql
+++ models/staging/movies.sql
@@ -1,19 +1,16 @@
-{{
-    config(
-        materialized='table',
-        pre_hook=[
+{ { config(
+    materialized = 'table',
+    pre_hook = [
             "{{model_movie_prehook()}}"
 
         ]
-    )
-}}
-
-select
-    movie_id,
+) } }
+select movie_id,
     title,
     industry,
     release_year,
     imdb_rating,
     studio,
-    language_id
-from {{ source('raw', 'raw_movies') }}
+    language_id,
+    year(current_timestamp()) - year(release_year) as movie_age
+from { { source('raw', 'raw_movies') } }
